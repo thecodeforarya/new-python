aa959a8218a4fb45d8de9d083a41a985718dfcce
thecodeforarya <ranaweb2255.com>
2024-02-11 00:40:40 +0530

    This is our project

---
+++ main.py
@@ -0,0 +1,33 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+def print_connected_hexagons(rows, col):
+    if col % 2 == 0:
+        col = col // 2
+        print(" ___""    " * col)
+        print("/   \\___" * col )
+        for i in range(rows):
+
+            print("\\___/   " * col + "\\")
+            if i == rows - 1:
+                print("    \\___/" + "   \\___/"* (col-1) )
+            else:
+               print("/   \\___" * col + "/")
+
+
+    else:
+        col = col // 2 + 1
+        print(" ___""    " * col)
+        for _ in range(rows):
+
+         print("/   \\___" * (col -1) + "/   \\")
+
+         print("\\___/   " * col)
+
+
+inputs = input("inputs: ").split()
+rows, col = map(int, inputs)
+print_connected_hexagons(rows, col)
